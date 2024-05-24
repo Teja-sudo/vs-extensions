531af771b776de416aa006c8b1ce5f110e8cee13
Teja <sanikommutejareddy@gmail.com>
2024-05-24 17:00:29 +0530

    minor change

--- FileChangesExplorer/src/extension.ts
+++ FileChangesExplorer/src/extension.ts
@@ -1,25 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { FilesChangesProvider } from "./tree";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const workspaceRoot =
+    vscode.workspace.workspaceFolders &&
+    vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : "";
+  const filesChangesProvider = new FilesChangesProvider(workspaceRoot);
+  vscode.window.registerTreeDataProvider("changesView", filesChangesProvider);
+  vscode.commands.registerCommand(
+    "my-changes-view.openFileAtLine",
+    (file: string, line: number) => {
+      openFileAtLine(file, line);
+    }
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "filechangesexplorerbysanik" is now active!');
+  vscode.commands.registerCommand("my-changes-view.refresh", () => {
+    // filesChangesProvider.refresh();
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('filechangesexplorerbysanik.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FileChangesExplorer!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "filechangesexplorerbysanik.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from FileChangesExplorer!"
+      );
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+}
+
+async function openFileAtLine(file: string, line: number) {
+  const document = await vscode.workspace.openTextDocument(file);
+  const editor = await vscode.window.showTextDocument(document);
+  const position = new vscode.Position(line - 1, 0);
+  editor.selection = new vscode.Selection(position, position);
+  editor.revealRange(new vscode.Range(position, position));
 }
 
 // This method is called when your extension is deactivated
--- FileChangesExplorer/src/tree.ts
+++ FileChangesExplorer/src/tree.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 
-export class NodeDependenciesProvider
+export class FilesChangesProvider
   implements vscode.TreeDataProvider<Dependency>
 {
   constructor(private workspaceRoot: string) {}
@@ -122,3 +122,188 @@ class Dependency extends vscode.TreeItem {
     ),
   };
 }
+
+`warning: in the working copy of 'FileChangesExplorer/src/extension.ts', LF will be replaced by CRLF the next time Git touches it
+diff --git a/FileChangesExplorer/src/extension.ts b/FileChangesExplorer/src/extension.ts
+index 497b17f..81b1d6b 100644
+--- a/FileChangesExplorer/src/extension.ts
++++ b/FileChangesExplorer/src/extension.ts
+@@ -3 +3 @@
+-import * as vscode from 'vscode';
++import * as vscode from "vscode";
+@@ -7,0 +8,5 @@ export function activate(context: vscode.ExtensionContext) {
++  // Use the console to output diagnostic information (console.log) and errors (console.error)
++  // This line of code will only be executed once when your extension is activated
++  console.log(
++    'Congratulations, your extension "filechangesexplorerbysanik" is now active!'
++  );
+@@ -9,3 +14,13 @@ export function activate(context: vscode.ExtensionContext) {
+-       // Use the console to output diagnostic information (console.log) and errors (console.error)
+-       // This line of code will only be executed once when your extension is activated
+-       console.log('Congratulations, your extension "filechangesexplorerbysanik" is now active!');
++  // The command has been defined in the package.json file
++  // Now provide the implementation of the command with registerCommand
++  // The commandId parameter must match the command field in package.json
++  let disposable = vscode.commands.registerCommand(
++    "filechangesexplorerbysanik.helloWorld",
++    () => {
++      // The code you place here will be executed every time your command is executed
++      // Display a message box to the user
++      vscode.window.showInformationMessage(
++        "Hello World from FileChangesExplorer!"
++      );
++    }
++  );
+@@ -13,8 +28,2 @@ export function activate(context: vscode.ExtensionContext) {
+-       // The command has been defined in the package.json file
+-       // Now provide the implementation of the command with registerCommand
+-       // The commandId parameter must match the command field in package.json
+-       let disposable = vscode.commands.registerCommand('filechangesexplorerbysanik.helloWorld', () => {
+-               // The code you place here will be executed every time your command is executed
+-               // Display a message box to the user
+-               vscode.window.showInformationMessage('Hello World from FileChangesExplorer!');
+-       });
++  context.subscriptions.push(disposable);
++}
+@@ -22 +31,6 @@ export function activate(context: vscode.ExtensionContext) {
+-       context.subscriptions.push(disposable);
++async function openFileAtLine(file: string, line: number) {
++  const document = await vscode.workspace.openTextDocument(file);
++  const editor = await vscode.window.showTextDocument(document);
++  const position = new vscode.Position(line - 1, 0);
++  editor.selection = new vscode.Selection(position, position);
++  editor.revealRange(new vscode.Range(position, position));
+diff --git a/FileChangesExplorer/src/tree.ts b/FileChangesExplorer/src/tree.ts
+index 98bdc3d..0e107be 100644
+--- a/FileChangesExplorer/src/tree.ts
++++ b/FileChangesExplorer/src/tree.ts
+@@ -5 +5 @@ import * as path from "path";
+-export class NodeDependenciesProvider
++export class FilesChangesProvider`;
+
+/*
+import { exec } from "child_process";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+interface FileChange {
+  filePath: string;
+  changes: { startLine: number; endLine: number }[];
+}
+
+export async function getChangedFilesDetails(): Promise<FileChange[]> {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (!workspaceFolders || workspaceFolders.length === 0) {
+    vscode.window.showErrorMessage("No workspace is currently open.");
+    return [];
+  }
+
+  const workspaceRoot = workspaceFolders[0].uri.fsPath;
+  try {
+    const { stdout } = await execAsync("git diff --unified=0", {
+      cwd: workspaceRoot,
+    });
+    return parseGitDiff(stdout);
+  } catch (error) {
+    vscode.window.showErrorMessage(`Failed to get changes: ${error.message}`);
+    return [];
+  }
+}
+
+function parseGitDiff(diffOutput: string): FileChange[] {
+  const fileChanges: FileChange[] = [];
+  const fileDiffs = diffOutput.split(/^diff --git a\/.* b\/.*$/gm).slice(1);
+
+  for (const fileDiff of fileDiffs) {
+    const lines = fileDiff.split("\n");
+    const filePathLine = lines.find((line) => line.startsWith("--- a/"));
+    if (!filePathLine) continue;
+
+    const filePath = filePathLine.replace("--- a/", "");
+    const changes: { startLine: number; endLine: number }[] = [];
+
+    const hunkHeaders = lines.filter((line) => line.startsWith("@@ "));
+    for (const hunkHeader of hunkHeaders) {
+      const match = /@@ -\d+(,\d+)? \+(\d+)(,(\d+))? @@/.exec(hunkHeader);
+      if (match) {
+        const startLine = parseInt(match[2], 10);
+        const lineCount = match[4] ? parseInt(match[4], 10) : 1;
+        const endLine = startLine + lineCount - 1;
+        changes.push({ startLine, endLine });
+      }
+    }
+
+    fileChanges.push({ filePath, changes });
+  }
+
+  return fileChanges;
+}
+*/
+
+/*
+import * as vscode from 'vscode';
+
+interface Change {
+    file: string;
+    lines: number[];
+}
+
+class ChangeItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        public readonly command?: vscode.Command
+    ) {
+        super(label, collapsibleState);
+    }
+}
+
+export class ChangesProvider implements vscode.TreeDataProvider<ChangeItem> {
+    private _onDidChangeTreeData: vscode.EventEmitter<ChangeItem | undefined | void> = new vscode.EventEmitter<ChangeItem | undefined | void>();
+    readonly onDidChangeTreeData: vscode.Event<ChangeItem | undefined | void> = this._onDidChangeTreeData.event;
+
+    private changes: Change[];
+
+    constructor() {
+        this.changes = this.getMockChanges();
+    }
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: ChangeItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(element?: ChangeItem): Thenable<ChangeItem[]> {
+        if (!element) {
+            return Promise.resolve(this.changes.map(change => 
+                new ChangeItem(change.file, vscode.TreeItemCollapsibleState.Collapsed)
+            ));
+        } else {
+            const change = this.changes.find(c => c.file === element.label);
+            if (change) {
+                return Promise.resolve(change.lines.map(line => 
+                    new ChangeItem(`Line ${line}`, vscode.TreeItemCollapsibleState.None, {
+                        command: 'my-changes-view.openFileAtLine',
+                        title: '',
+                        arguments: [change.file, line]
+                    })
+                ));
+            } else {
+                return Promise.resolve([]);
+            }
+        }
+    }
+
+    private getMockChanges(): Change[] {
+        return [
+            { file: 'src/file1.ts', lines: [10, 20, 30] },
+            { file: 'src/file2.ts', lines: [15, 25] }
+        ];
+    }
+}
+
+*/
